762e34eddb828db079fb7c0a5273654f34ccda90
yehia67 <yehiatarek67@gmail.com>
2019-03-03 20:19:57 +0200

    pi in normal serial program

--- pi.c
+++ pi.c
@@ -2,14 +2,19 @@
 //./a.out
 #include <omp.h>
 #include <stdio.h>
+long num_steps = 1000000;
+//Program To Calculate Pi value
 int main()
 {
-        int x = omp_get_max_threads();
-        printf("omp run with max number of threads = %d \n",x);
-        #pragma omp parallel
+        int i;
+        double x,sum=0.0;
+        double pi,step = (double) 1/num_steps;
+        for( i = 0; i < num_steps; i++)
         {
-       
-        printf(" yarab = thread num %d \n",omp_get_thread_num());
+                x = (double) (i+ 0.5)* step;
+                sum = sum + 4.0/(1.0+x*x);
         }
+        pi = step * sum;
+        printf("%f\n",pi);
 return 0;
 }
