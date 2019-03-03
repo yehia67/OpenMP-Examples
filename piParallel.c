83233028f4711eb577c13284f48ab7747ff9c754
yehia67 <yehiatarek67@gmail.com>
2019-03-03 20:55:50 +0200

    Pi program and Parallel Pi

--- piParallel.c
+++ piParallel.c
@@ -3,18 +3,24 @@
 #include <omp.h>
 #include <stdio.h>
 long num_steps = 1000000;
-//Normal Serial Program To Calculate Pi value
+//Program To Calculate Pi value
 int main()
 {
         int i;
-        double x,sum=0.0;
-        double pi,step = (double) 1/num_steps;
+        double sum=0.0;
+        double x;
+        double pi,step = (double) 1.0/num_steps;
+        #pragma omp parallel
+        {
+        double x;
+        #pragma omp parallel for reduction(+:sum)
         for( i = 0; i < num_steps; i++)
         {
                 x = (double) (i+ 0.5)* step;
                 sum = sum + 4.0/(1.0+x*x);
         }
         pi = step * sum;
+        }
         printf("%f\n",pi);
 return 0;
 }
